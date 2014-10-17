ea6eb73b70170168c3bb4cd1ee359e2b3535ebce
vargas29 <mavar09@gmail.com>
2014-10-17 19:16:59 +0200

    Revert "Revert "mitad""
    This reverts commit 44387ed70b10926705939e343e3b3a8954d89fd9.

--- controladores/producto.php
+++ controladores/producto.php
@@ -2,20 +2,33 @@
 
   require_once("../Modelo/DAO/ProductoDAO.php");
 
-   $objproveedor = new producto();
-   $ProveedorDao = new ProductoDao;
+   $objproducto = new producto();
+   $productoDao = new ProductoDao;
 	
-    $objproveedor->setNit($_POST['nit']);
-    $objproveedor->setDireccion($_POST['direccion']);
-    $objproveedor->setTelefono($_POST['telefono']);
-    $objproveedor->setEmail($_POST['email']);
-    $objproveedor->setWeb($_POST['web']);
-    $objproveedor->setContac($_POST['contac']);
-    $objproveedor->setReplegal($_POST['replegal']);
-    $objproveedor->setcedRep($_POST['cedrep']);
-    $objproveedor->setcelRep($_POST['celrep']);
+    $objproducto->setid_categoria($_POST['id_categoria']);
+    $objproducto->setid_subcategoria($_POST['id_subcategoria']);
+    $objproducto->setid_proveedor($_POST['id_proveedor']);
+    $objproducto->setdescripcion($_POST['descripcion']);
+    $objproducto->setv_costo($_POST['v_costo']);
+    $objproducto->setventa_1($_POST['venta_1']);
+    $objproducto->setventa_2($_POST['venta_2']);
+    $objproducto->setventa_3($_POST['venta_3']);
+    $objproducto->setventa_4($_POST['venta_4']);
+    $objproducto->setp_utilidad($_POST['p_utilidad']);
+    $objproducto->setstock_min($_POST['stock_min']);
+    $objproducto->setstock_max($_POST['stock_max']);
+    $objproducto->setmedida($_POST['medida']);
+    $objproducto->setp_descuento($_POST['p_descuento']);
+    $objproducto->setobservacion($_POST['observacion']);
+    $objproducto->setfcreado($_POST['fcreado']);
+    $objproducto->setfmodificado($_POST['fmodificado']);
 
 
-    $ProveedorDao->guardar($objproveedor);
+
+
+
+
+
+    $productoDao->guardar($objproducto);
 
 ?>
--- vista/js/formulario_producto.js
+++ vista/js/formulario_producto.js
@@ -1,26 +1,29 @@
 jQuery(document).ready(function() {
-  
-    $("#btn").click(function() {
-     var _id_categoria=$("#txtid_categoria").val();
-     var _id_subcategoria= $("#txtid_subcategoria").val();
-     var _id_proveedor=$("#txtid_proveedor").val();
-     var _descripcion=$("#txtdescripcion").val();
-     var _v_costo= $("#txtv_costo").val();
-     var _venta_1= $("#txtventa_1").val();
-     var _venta_2=$("#txtventa_2").val();
-     var _venta_3= $("#txtventa_3").val();
-     var _venta_4= $("#txtventa_4").val();
-     var _p_utilidad= $("#txtp_utilidad").val();
-     var _stock_min= $("#txtstock_min").val();
-     var _stock_max= $("#txtstock_max").val();
-     var _medida= $("#txtmedida").val();
-     var _p_descuento= $("#txtp_descuento").val();
-     var _observacion= $("#txtobservacion").val();
+  alert("kjkjb");
+  $("#btn").click(function() {
+    var _id_categoria=$("#txtid_categoria").val();
+    var _id_subcategoria= $("#txtid_subcategoria").val();
+    var _id_proveedor=$("#txtid_proveedor").val();
+    var _descripcion=$("#txtdescripcion").val();
+    var _v_costo= $("#txtv_costo").val();
+    var _venta_1= $("#txtventa_1").val();
+    var _venta_2= $("#txtventa_2").val();
+    var _venta_3= $("#txtventa_3").val();
+    var _venta_4= $("#txtventa_4").val();
+    var _p_utilidad= $("#txtp_utilidad").val();
+    var _stock_min= $("#txtstock_min").val();
+    var _stock_max= $("#txtstock_max").val();
+    var _medida= $("#txtmedida").val();
+    var _p_descuento= $("#txtp_descuento").val();
+    var _observacion= $("#txtobservacion").val();
+    var d = new Date(); 
+    $fecha =(d.getDate()+"/"+d.getMonth()+"/"+d.getFullYear()+" , "+d.getHours()+":"+d.getMinutes());
+    var _fcreado=$fecha;
+    var _fmodificado=$fecha;
 
-     var parametros={ id_categoria : _id_categoria , id_subcategoria: _id_subcategoria , id_proveedor : _id_proveedor , descripcion :_descripcion , v_costo : _v_costo , venta_ 1: _venta_1 ,venta_2 :_venta_2 , venta_3:_venta_3 , venta_4:_venta_4 ,p_utilidad:_p_utilidad , _stock_min:_stock_min, stock_max : _stock_max, medida : _medida, p_descuento : _p_descuento, observacion : _observacion };
-     var archivo='../controladores/producto.php';
-   
-     $.ajax({
+    var parametros={ id_categoria : _id_categoria , id_subcategoria: _id_subcategoria , id_proveedor : _id_proveedor , descripcion :_descripcion , v_costo : _v_costo , venta_1: _venta_1 ,venta_2 :_venta_2 , venta_3:_venta_3 , venta_4:_venta_4 ,p_utilidad:_p_utilidad , stock_min:_stock_min, stock_max : _stock_max, medida : _medida, p_descuento : _p_descuento, observacion : _observacion,fcreado:_fcreado,fmodificado:_fmodificado };
+    var archivo='../controladores/producto.php';
+    $.ajax({
       type :"POST",
       url : archivo,
       data :parametros,
@@ -28,6 +31,6 @@ jQuery(document).ready(function() {
         $("#respuesta").html(datos);
       }
     });
-    });
-    
+  });
+
 });
