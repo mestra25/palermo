d601d4dfa51aba880fbc56b39a8dedf469d9bd3f
vargas29 <mavar09@gmail.com>
2014-11-04 22:13:06 -0500

    dfghj
    dfgh

--- vista/js/formulario_proveedor.js
+++ vista/js/formulario_proveedor.js
@@ -50,6 +50,53 @@ jQuery(document).ready(function()
           
       } });
 
+$("#guardarProvee").validate({
+        rules: {
+            txtcedula: { required: true, minlength: 6},
+            txtnombre: { required: true,maxlength: 15},
+            txtapellido: { required:true, maxlength: 15},
+            txtcelular: {required:true,   maxlength: 15 },
+            txtdireccion: { required: true, maxlength: 30},
+            txtemail: {  email:true,required:true},
+            
+            
+        },
+        messages: {
+            txtcedula: "Debe indicar su cedula",
+            txtnombre: "Debe  indicar su Nombre",
+            txtapellido:"Debe  indicar su Apellido",
+            txtcelular: "Debe indicar su telefono , solo numeros",
+            txtdireccion: "Indique una direccion",
+            txtemail: "Debe indicar un formato correcto",
+          
+          },
+        submitHandler: function(form){
+        var _nit=$("#txtnit").val();
+     var _nombre_empresa=$("#txtnombre_empresa").val();
+     var _direccion= $("#txtdireccion").val();
+     var _telefono=$("#txttelefono").val();
+     var _email=$("#txtemail").val();
+     var _web= $("#txtweb").val();
+     var _contac= $("#txtcontac").val();
+     var _numcontac= $("#txtnumcontac").val();
+     var _replegal=$("#txtreplegal").val();
+     var _cedrep= $("#txtcedrep").val();
+     var _celrep= $("#txtcelrep").val();
+
+     var parametros={ nit : _nit, nombre_empresa : _nombre_empresa, direccion : _direccion , telefono : _telefono , email :_email , web : _web , contac : _contac,numcontac:_numcontac ,replegal :_replegal , cedrep:_cedrep , celrep:_celrep};
+     var archivo='../controladores/proveedor.php?action=Editar';
+   
+     $.ajax({
+      type :"POST",
+      url : archivo,
+      data :parametros,
+      success : function( datos){
+        $("#rpt").html(datos);
+      }
+    });
+          
+      } });
+
 
      $("#btn_pro").click(function() {
 
@@ -80,33 +127,7 @@ jQuery(document).ready(function()
 
 
 
-    $(".btneditar").click(function  () {
-     var _nit=$("#txtnit").val();
-     var _nombre_empresa=$("#txtnombre_empresa").val();
-     var _direccion= $("#txtdireccion").val();
-     var _telefono=$("#txttelefono").val();
-     var _email=$("#txtemail").val();
-     var _web= $("#txtweb").val();
-     var _contac= $("#txtcontac").val();
-     var _numcontac= $("#txtnumcontac").val();
-     var _replegal=$("#txtreplegal").val();
-     var _cedrep= $("#txtcedrep").val();
-     var _celrep= $("#txtcelrep").val();
-
-     var parametros={ nit : _nit, nombre_empresa : _nombre_empresa, direccion : _direccion , telefono : _telefono , email :_email , web : _web , contac : _contac,numcontac:_numcontac ,replegal :_replegal , cedrep:_cedrep , celrep:_celrep};
-     var archivo='../controladores/proveedor.php?action=Editar';
-   
-     $.ajax({
-      type :"POST",
-      url : archivo,
-      data :parametros,
-      success : function( datos){
-        $("#rpt").html(datos);
-      }
-    });
-
     
-    });
     
 
   
