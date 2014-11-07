7d68e5dbe436dbfaa791156dc718d7fab2524bdd
vargas29 <mavar09@gmail.com>
2014-11-07 11:27:01 -0500

    Compras

--- vista/compras.php
+++ vista/compras.php
@@ -1,164 +1,55 @@
-<?php
 
-session_start();
-
-if ($_SESSION['administrador']=="si") {
-  
-?>
-
-<!DOCTYPE html>
-<html lang="es">
-
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-  <title>DataSistemas</title>
-  <link rel="stylesheet" href="css/tabla_inventario.css">
-
-  <!-- Bootstrap Core CSS -->
-  <link href="css/bootstrap.min.css" rel="stylesheet"> 
-
-  <!-- Custom CSS -->
-  <link href="css/business-casual.css" rel="stylesheet">
-
-  <!-- Fonts -->
-
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Guardar checkbox</title>
 </head>
 
 <body>
-  <div id="dialogoverlay"></div>
-  <div id="dialogbox">
-    <div>
-      <div id="dialogboxhead"></div>
-      <div id="dialogboxbody"></div>
-      <div id="dialogboxfoot"></div>
-    </div>
-  </div>
+<table border="1">
+  <tr>
+    <td colspan="2">Compras</td>
+    
+  </tr>
+  <tr>
+    <td><label for="name">Proveedor:  </label></td>
+    <td> <select id="txtid_proveedor" title="Se requiere un Proveedor" required class="campos_edi">          
 
-  <div class="brand" id="img"><a href=""><img src="img/logo.png"></a></div>
+        <option value="" >Seleccionesun proveedor</option>
+       <option value="nueva_proveedor">Nuevo Proveedor</option>
 
-  <!-- Navigation -->
-  <nav class="navbar navbar-default" role="navigation">
-    <div class="container">
-      <!-- Brand and toggle get grouped for better mobile display -->
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <!-- navbar-brand is hidden on larger screens, but visible when the menu is collapsed -->
-        <a class="navbar-brand" href="index.html">Data Sistemas</a>
-      </div>
-      <!-- Collect the nav links, forms, and other content for toggling -->
-      <center>
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul class="nav navbar-nav">
-            <li>
-              <a href="index.html">Inicio</a>
-            </li>
-            <li>
-              <a href="administrar.php">Administrar</a>
-            </li>
-            <li>
-              <a href="usuarios.php">Usuarios</a>
-            </li>
-            <li>
-              <a href="inventario.php">Inventario</a>
-            </li>
-            <li>
-              <a href="cliente.php">Clientes</a>
-            </li>
-            <li>
-            <a href="logout.php">Salir</a>
-            </li>
-          </ul>
-        </div>
-      </center>
-      <!-- /.navbar-collapse -->
-    </div>
-    <!-- /.container -->
-  </nav>
-    <div class="">
+        <?php
+        require("php/lista_proveedor.php");
 
-        <div class="contenido">
-            <div>
-                <center>
-                    <ul class="nav navbar-nav">
-                        <li>
-                            <a href="inventario.php">Reservas</a>
-                        </li>
-                        <li>
-                            <a href="compras.php">Compras</a>
-                        </li>
-                        <li>
-                            <a href="act_inventario.php">Act. Inventario</a>
-                        </li>
-                        </ul>
-                </center>
-            </div>
-        </div>
+        foreach ($consulta as $registro) {
 
-    </div><!-- end contenedor-->
-  <div class="">
+         echo " <option value=".$registro['id_proveedor'].">".$registro['nombre_empresa']."</option>";
+       }
+       ?>
 
-   <div class="contenido">
+     </select>  </td>
+  </tr>
+  <tr>
+    <td>Productos</td>
+    <td> <a href="">Mas Productos<img src=""></a></td>
+     <td> <?php
+			require("php/lista_producto.php");
+			foreach ($consulta as $registro)
+			 {
+			 $id_cat=$registro['id_categoria'];
+			 $id_sub=$registro['id_subcategoria'];
+			   
+			 require("php/p_categoria.php");
+			 require("php/p_subcategoria.php");
+			 echo " <input name='checkbox[]' type='checkbox' value=".$registro_1['descripcion']." ".$registro_2['descripcion']." ".$registro['descripcion']."/>";             
+			
+			}
+			?></td>
+  </tr>
+</table>
 
-   <div id="tabla">
-
-
-    <p>Compras</p>
-
-  </div>
-</div>
-
-
-
-<span id="respuesta"></span>
-</div><!-- end contenedor-->
-
-
-<footer>
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-12 text-center">
-        <p>Copyright &copy; Diego Narvaez - Maria Fernanda Mendoza 2014 </p>
-
-      </div>
-    </div>
-  </div>
-</footer>
-
-<!-- jQuery Version 1.11.0 -->
-<script src="js/jquery-1.11.0.js"></script>
-
-<!-- Bootstrap Core JavaScript -->
-<script src="js/bootstrap.min.js"></script>
-
-<!-- Script to Activate the Carousel -->
-<script>
-$('.carousel').carousel({
-        interval: 5000 //changes the speed
-      })
-</script>
-<script type="text/javascript" src="js/formulario_inventario.js"></script>
-<script type="text/javascript" src="js/buscar.js"></script>
-<script type="text/javascript" src="js/mostrar_ocultar.js"></script>
+ 
 
 </body>
-
 </html>
-<?php
-
-}else {
-
-  header("location:login.html");
-}
-
-?>
